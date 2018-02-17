8c9659b26b893deb416faafe78b0a218f0acedf9
dvram1 <36546367+dvram1@users.noreply.github.com>
2018-02-17 18:34:34 +0530

    Sample files
    Sample cource and test files

---
+++ PickList.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Atlas.Service.Contracts;
+
+namespace Atlas.Service.MediaFrame.Common
+{
+    public class PickList
+    {
+        /// <summary>
+        /// A constant string value representing a group containing all the 
+        /// non-expensive properties.
+        /// </summary>
+        public const string Inexpensive = "Inexpensive";
+
+        /// <summary>
+        /// A constant string value representing a group containing all the 
+        /// non-private properties.
+        /// </summary>
+        public const string AllFields = "All";
+
+        public const char PickListSeparator = '+';
+
+        private readonly string _pickList;
+        private string _barePickList;
+        private IEnumerable<string> _barePickListCollection;
+        private string _fullyQualifiedPickList;
+        private IEnumerable<string> _fullyQualifiedPickListCollection;
+        private readonly object _lockObject = new object();
+
+        public PickList(string pickList)
+        {
+            _pickList = pickList;
+        }
+
+        public PickList(IEnumerable<string> pickList)
+        {
+            if (pickList != null && pickList.Any())
+            {
+                _pickList = string.Join(PickListSeparator.ToString(), pickList);
+            }
+        }
+
+        public IEnumerable<string> FullyQualifiedPickListCollection
+        {
+            get
+            {
+                if (_fullyQualifiedPickListCollection == null)
+                {
+                    throw new ApplicationException("PickList has not been normalized");
+                }
+
+                return _fullyQualifiedPickListCollection;
+            }
+        }
+
+        public IEnumerable<string> BarePickListCollection
+        {
+            get
+            {
+                if (_barePickListCollection == null)
+                {
+                    throw new ApplicationException("PickList has not been normalized");
+                }
+
+                return _barePickListCollection;
+            }
+        }
+
+        public string FullyQualifiedPickList
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_fullyQualifiedPickList))
+                {
+                    throw new ApplicationException("PickList has not been normalized");
+                }
+
+                return _fullyQualifiedPickList;
+            }
+        }
+
+        public string BarePickList
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_barePickList))
+                {
+                    throw new ApplicationException("PickList has not been normalized");
+                }
+
+                return _barePickList;
+            }
+        }
+
+        public void Normalize(IEnumerable<Field> entityFields)
+        {
+            // materialize the entityFields collection if it is an enumerator rather than a collection.
+            if (!(entityFields is IList<Field>))
+            {
+                entityFields = entityFields.ToArray();
+            }
+
+            lock (_lockObject)
+            {
+                _fullyQualifiedPickList = NormalizePickList(_pickList, entityFields);
+                _fullyQualifiedPickListCollection = FullyQualifiedPickList.Split(PickListSeparator);
+                _barePickListCollection = FullyQualifiedPickListCollection.Select(x => DequalifyPickListField(x)).ToArray();
+                _barePickList = string.Join(PickListSeparator.ToString(), BarePickListCollection);
+            }
+        }
+
+        private static string DequalifyPickListField(string pickListField)
+        {
+            string dequalified = pickListField.Substring(pickListField.LastIndexOf('.') + 1);
+            return dequalified;
+        }
+
+        public void AddField(Field field)
+        {
+            lock (_lockObject)
+            {
+                if (!FullyQualifiedPickListCollection.Contains(field.Name, StringComparer.CurrentCultureIgnoreCase))
+                {
+                    _barePickListCollection = _barePickListCollection.Union(new string[] { field.FieldName }).ToArray();
+                    _barePickList = string.Join(PickListSeparator.ToString(), _barePickList, field.FieldName);
+                    _fullyQualifiedPickListCollection = _fullyQualifiedPickListCollection.Union(new string[] { field.Name }).ToArray();
+                    _fullyQualifiedPickList = string.Join(PickListSeparator.ToString(), _fullyQualifiedPickList, field.Name);
+                }
+            }
+        }
+
+        public static string NormalizePickList(string pickList, IEnumerable<Field> fields)
+        {
+            pickList = string.IsNullOrWhiteSpace(pickList) ? AllFields : pickList;
+
+            IEnumerable<string> notNormalizedPicks =
+                pickList
+                    .Split('+')
+                    .Where(x => !string.IsNullOrWhiteSpace(x))
+                    .Distinct(StringComparer.CurrentCultureIgnoreCase)
+                    .Select(x => x.Trim());
+
+            HashSet<string> normalizedPickList = new HashSet<string>();
+
+            foreach (string pick in notNormalizedPicks)
+            {
+                if (StringComparer.CurrentCultureIgnoreCase.Equals(pick, AllFields))
+                {
+                    IEnumerable<string> allFieldNames = fields
+                        .Where(x => !(x.FieldName == MarkerHarmonizedName.MappedId && x.EntityName == "Marker"))
+                        .Select(x => x.Name)
+                        .Distinct(StringComparer.CurrentCultureIgnoreCase);
+                    return string.Join("+", allFieldNames);
+                }
+                else if (StringComparer.CurrentCultureIgnoreCase.Equals(pick, Inexpensive))
+                {
+                    IEnumerable<string> inexpensiveFields =
+                        fields
+                            .Where(x => !x.IsExpensive 
+                                && !x.IsPrivate 
+                                && !(x.FieldName == MarkerHarmonizedName.MappedId && x.EntityName == "Marker"))
+                            .Select(x => x.Name)
+                            .Distinct(StringComparer.CurrentCultureIgnoreCase);
+
+                    foreach (string inexpensiveField in inexpensiveFields)
+                    {
+                        normalizedPickList.Add(inexpensiveField);
+                    }
+                }
+                else
+                {
+                    string normalizedField = NormalizePickListField(fields, pick);
+                    normalizedPickList.Add(normalizedField);
+                }
+            }
+
+            return string.Join("+", normalizedPickList);
+        }
+
+        public static string NormalizePickListField(IEnumerable<Field> fields, string field)
+        {
+            if (string.IsNullOrWhiteSpace(field))
+            {
+                throw new ArgumentException("field cannot be null/empty", "field");
+            }
+
+            if (StringComparer.CurrentCultureIgnoreCase.Equals(field, Inexpensive) || StringComparer.CurrentCultureIgnoreCase.Equals(field, AllFields))
+            {
+                return field;
+            }
+
+            string[] pick = field.Split('.');
+            if (pick.Length < 3)
+            {
+                IEnumerable<Field> matches = fields;
+                if (pick.Length == 2)
+                {
+                    matches = matches.Where(x =>
+                        StringComparer.CurrentCultureIgnoreCase.Equals(x.EntityName, pick.First()) ||
+                        StringComparer.CurrentCultureIgnoreCase.Equals(x.SchemaName, pick.First()));
+                }
+
+                matches = matches.Where(x =>
+                    StringComparer.CurrentCultureIgnoreCase.Equals(x.FieldName, pick.Last()));
+
+                if (matches.Any())
+                {
+                    if (matches.Count() == 1)
+                    {
+                        field = matches.First().Name;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(string.Format("[Pick List] Ambiguous field: '{0}'", field));
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException(string.Format("[Pick List] Field not found: '{0}'", field));
+                }
+            }
+            else if (pick.Length > 3)
+            {
+                throw new ArgumentException(string.Format("[Pick List] Invalid field: '{0}'; field must be specified as <schema>.<entity>.<field>.", field));
+            }
+            else if (!fields.Any(x => StringComparer.CurrentCultureIgnoreCase.Equals(field, x.Name)))
+            {
+                // mediaframe name may be specified instead of atlas name; try to resolve
+                var matches = fields.Where(x =>
+                    StringComparer.CurrentCultureIgnoreCase.Equals(x.SchemaName, pick[0]) &&
+                    StringComparer.CurrentCultureIgnoreCase.Equals(x.EntityName, pick[1]) &&
+                    StringComparer.CurrentCultureIgnoreCase.Equals(x.FieldName, pick[2]));
+
+                if (matches.Count() == 1)
+                {
+                    field = matches.First().Name;
+                }
+                else if (matches.Count() > 1)
+                {
+                    throw new ArgumentException(string.Format("[Pick List] Ambiguous field: '{0}'", field));
+                }
+                else
+                {
+                    throw new ArgumentException(string.Format("[Pick List] Field not found: '{0}'", field));
+                }
+            }
+
+            return field;
+        }
+
+        public bool Contains(string value)
+        {
+            string[] currentValues = _pickList.Split(PickListSeparator);
+            string[] checkValues = value.Split(PickListSeparator);
+
+            if (currentValues.Intersect(checkValues).Any())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ PicklistTests.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlas.Service.MediaFrame.Common;
+using NUnit.Framework;
+using Microsoft.Practices.Unity;
+using Atlas.Service.Contracts;
+using Atlas.Service.MediaFrame;
+using FakeItEasy;
+
+namespace Atlas.Tests.Common
+{
+    [TestFixture]
+    class PicklistTests 
+    {
+        private FieldList GetFields()
+        {
+            Field field1 = new Field() { SchemaName = "atlas", EntityName = "Asset", PublicName = "PN1", FieldName = "Field1", Type = FieldType.String, IsExpensive = false, IsPrivate = false };
+            Field field2 = new Field() { SchemaName = "atlas", EntityName = "Asset", PublicName = "PN2", FieldName = "Field2", Type = FieldType.String, IsExpensive = false, IsPrivate = false };
+            Field field3 = new Field() { SchemaName = "atlas", EntityName = "Asset", PublicName = "PN3", FieldName = "Field3", Type = FieldType.String, IsExpensive = true, IsPrivate = false };
+            Field field4 = new Field() { SchemaName = "atlas", EntityName = "Asset", PublicName = "PN4", FieldName = "Field4", Type = FieldType.String, IsExpensive = false, IsPrivate = true };
+            Field field5 = new Field() { SchemaName = "atlas", EntityName = "Asset", PublicName = "PN5", FieldName = "Field5", Type = FieldType.String, IsExpensive = false, IsPrivate = false };
+
+            return new FieldList { field1, field2, field3, field4, field5 };
+        }
+
+        [Test]
+        public void TestNormalize()
+        {
+            FieldList fieldList = GetFields();
+            PickList picklist = new PickList("Field1+Field2");
+  
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 2);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 2);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2");
+        }
+
+        [Test]
+        public void TestNormalizewithAnotherConstructor()
+        {
+            FieldList fieldList = GetFields();
+            IEnumerable<string> strings = new string[]{ "Field1", "Field2" };
+            PickList picklist = new PickList(strings);
+
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 2);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 2);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2");
+        }
+
+        [Test]
+        public void TestNormalizewithEmptyPicklistvalue()
+        {
+            FieldList fieldList = GetFields();
+            PickList picklist = new PickList("");
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.Field3+atlas.Asset.Field4+atlas.Asset.Field5");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 5);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 5);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2+Field3+Field4+Field5");
+        }
+
+        [Test]
+        public void TestNormalizewithFQPicklist()
+        {
+            FieldList fieldList = GetFields(); 
+            PickList picklist = new PickList("atlas.Asset.Field1+atlas.Asset.Field2");
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 2);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 2);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2");
+        }
+
+        [Test]
+        public void TestNormalizewithEntityFieldNamePicklist()
+        {
+            FieldList fieldList = GetFields();
+            PickList picklist = new PickList("Asset.Field1+Asset.Field2");
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 2);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 2);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2");
+        }
+
+        [Test]
+        public void TestNormalizewithAllFieldsinPicklist()
+        {
+            FieldList fieldList = GetFields();
+            PickList picklist = new PickList("Field1+Field2+" + PickList.AllFields);
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.Field3+atlas.Asset.Field4+atlas.Asset.Field5");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 5);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 5);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2+Field3+Field4+Field5");
+        }
+
+        [Test]
+        public void TestNormalizewithInexpensiveinPicklist()
+        {
+            FieldList fieldList = GetFields();
+            PickList picklist = new PickList("Field1+Field2+" + PickList.Inexpensive);
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.Field5");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 3);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 3);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2+Field5");
+        }
+
+
+        [Test]
+        public void TestNormalizePickList()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickList("Field1+Field2", fieldList);
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+        }
+
+        [Test]
+        public void TestNormalizePickListwithEmptyPicklistvalue()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickList("", fieldList);
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.Field3+atlas.Asset.Field4+atlas.Asset.Field5");
+        }
+
+        [Test]
+        public void TestNormalizePickListwithFQPicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickList("atlas.Asset.Field1+atlas.Asset.Field2", fieldList);
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+        }
+
+        [Test]
+        public void TestNormalizePickListwithEntityFieldNamePicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickList("Asset.Field1+Asset.Field2", fieldList);
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+        }
+
+        [Test]
+        public void TestNormalizePickListwithAllFieldsinPicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickList("Field1+Field2+" + PickList.AllFields, fieldList);
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.Field3+atlas.Asset.Field4+atlas.Asset.Field5");
+        }
+
+        [Test]
+        public void TestNormalizePickListwithInexpensiveinPicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickList("Field1+Field2+" + PickList.Inexpensive, fieldList);
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.Field5");
+        }
+
+        [Test]
+        public void TestAddFieldField()
+        {
+            FieldList fieldList = GetFields();
+            PickList picklist = new PickList("Field1+Field2");
+
+            picklist.Normalize(fieldList);
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 2);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 2);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2");
+
+            Field newfield = new Field() { SchemaName = "atlas", EntityName = "Asset", PublicName = "NewField", FieldName = "NewField", Type = FieldType.String, IsExpensive = false, IsPrivate = false };
+            picklist.AddField(newfield);
+
+            Assert.AreEqual(picklist.FullyQualifiedPickList, "atlas.Asset.Field1+atlas.Asset.Field2+atlas.Asset.NewField");
+            Assert.IsTrue(picklist.FullyQualifiedPickListCollection.Count() == 3);
+            Assert.IsTrue(picklist.BarePickListCollection.Count() == 3);
+            Assert.AreEqual(picklist.BarePickList, "Field1+Field2+NewField");
+        }
+
+        [Test]
+        public void TestNormalizePickListField()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickListField(fieldList,"Field1");
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNormalizePickListFieldwithEmptyPicklistvalue()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickListField(fieldList, "");
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1");
+        }
+
+        [Test]
+        public void TestNormalizePickListFieldwithFQPicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickListField(fieldList, "atlas.Asset.Field1");
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1");
+        }
+
+        [Test]
+        public void TestNormalizePickListFieldwithEntityFieldNamePicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickListField(fieldList, "Asset.Field1");
+            Assert.AreEqual(normalizedPickList, "atlas.Asset.Field1");
+        }
+
+        [Test]
+        public void TestNormalizePickListFieldwithAllFieldsinPicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickListField(fieldList, PickList.AllFields);
+            Assert.AreEqual(normalizedPickList, "All");
+        }
+
+        [Test]
+        public void TestNormalizePickListFieldwithInexpensiveinPicklist()
+        {
+            FieldList fieldList = GetFields();
+            string normalizedPickList = PickList.NormalizePickListField(fieldList, PickList.Inexpensive);
+            Assert.AreEqual(normalizedPickList, "Inexpensive");
+        }
+
+
+        [Test]
+        public void TestContains()
+        {
+            FieldList fieldList = GetFields();
+            IEnumerable<string> strings = new string[] { "Field1", "Field2" };
+            PickList picklist = new PickList(strings);
+            
+            Assert.IsTrue(picklist.Contains("Field1"));
+            Assert.IsFalse(picklist.Contains("Field25"));
+        }
+
+    }
+}
